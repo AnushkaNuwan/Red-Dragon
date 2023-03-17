913c130f71016cae93b6a37ba1bcf23a8e21c4fe
AnushkaNuwan <115855498+AnushkaNuwan@users.noreply.github.com>
2023-03-17 17:32:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,8 +11,11 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'your key',
 }
 
-global.alivemsg = `📤I am alive now😇
-🤷‍♀️How can I help you?😉` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `📤I AM RED DRAGON BOT
+🗼CREATE BY ANUSHKA NUWAN
+🖲️I AM ALIVE NOW
+⛩️YOU CAN USE ME🤭
+🤷‍♂️How can I help you?😉` //Costomize Alive Message (type your message in `` )
 
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
@@ -29,9 +32,9 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
-global.ytname = "" // yt chanel name
+global.vcardowner = ['+94766588513'] // owner number
+global.ownername = "𝐀𝐍𝐔𝐒𝐇𝐊𝐀 𝐍𝐔𝐖𝐀𝐍" // owner name
+global.ytname = "Nuwan Priyantha 01" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
@@ -47,16 +50,16 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
+global.owner = ['+94766588513']
+global.ownertag = '+94766588513' //your tag number
+global.botname = '༆𝐑𝐄𝐃༒𝐃𝐑𝐀𝐆𝐎𝐍༒𝐁𝐎𝐓࿐' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.author = "'༆𝐑𝐄𝐃༒𝐃𝐑𝐀𝐆𝐎𝐍࿐" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+94766588513'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
